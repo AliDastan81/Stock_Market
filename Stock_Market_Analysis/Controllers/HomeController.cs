c3ebdd77814447518e4f34472af057639af6fbf8
Dastan <alial@ALI>
2025-04-21 22:20:00 +0330

    start project

---
+++ Stock_Market_Analysis/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stock_Market_Analysis.Models;
+using System.Diagnostics;
+
+namespace Stock_Market_Analysis.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int k = 1;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            int z = 8;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            int f = 87;
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ Stock_Market_Analysis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Stock_Market_Analysis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
